b1491a1af7f6937b88de0031aad412713ca4e820
Freddy Jimenez <freddy.junior@gmail.com>
2020-10-14 21:52:26 -0500

    adds city results and event listener for added city results

--- js/script.js
+++ js/script.js
@@ -1,24 +1,94 @@
 // ***** VARIABLES AND CONSTANTS *****
+let cityResults;
+let allCityResultsArray = [];
+let CITY_INPUT;
 
-
-
+const CITYAPI = {
+  dataType: 'json',
+  url: `https://developers.zomato.com/api/v2.1/cities?q=${CITY_INPUT}`,
+  headers: {
+    'user-key': '23701d91eca9f047dee1c0d369818987'
+  },
+}
 // ***** CACHED ELEMENT REFERENCES *****
+let $form = $('form');
+let $input = $('input[type="text"]');
+let $mainTitle = $('#mainTitle');
+let $listEl = $('#list');
+let $cityResult;
 
 
 
 // ***** EVENT LISTENERS *****
-
+$form.on('submit', handleGetCity);
 
 
 // ***** FUNCTIONS *****
+function handleGetCity(event) {
+  event.preventDefault();
+  CITY_INPUT = $input.val();
 
+  // EMPTIES ARRAY AFTER EACH SUBMIT
+  allCityResultsArray = [];
 
+  // ONLY RUNS CITY API IF THERE IS AN INPUT
+  if (CITY_INPUT) {
+    $.ajax({
+      dataType: 'json',
+      url: `https://developers.zomato.com/api/v2.1/cities?q=${CITY_INPUT}`,
+      headers: {
+      'user-key': '23701d91eca9f047dee1c0d369818987'
+      }
+    })
+    .then(function(response) {
+    // ASSIGNS RESPONSE TO VARIABLE
+    cityResults = response;
+    // RUNS DEFINE DATA FUNCTION
+    defineData(true);
+    // RENDERS CITY RESULTS
+    cityResultRender();
+    }, function(error) {
+      console.log('ERROR', error);
+    });
+  } else return;
+}
 
+function defineData(city) {
+  // CITY DATA
+  if(city) {
+    cityResults.location_suggestions.forEach(function(index) {
+      allCityResultsArray.push(index.name);
+    })
+    console.log('RESULTS', allCityResultsArray);
+  }
+}
 
+function cityResultRender() {
+  // FADES OUT INPUT BAR
+  $form.fadeOut(100);
+  // CHANGES TITLE TO CHOOSE YOUR CITY
+  $mainTitle.text(`Choose your city:`);
+  // GENERATES LIST OF CITIES FROM RESPONSE ONTO PAGE
+  $listEl.html(generateCityList());
 
+  // ***** CITY RESULT CACHED REFERENCE AND EVENT LISTENER *****
+  $cityResult = $('.cityResult');
+  $cityResult.on('click', handleGetCuisine);
+}
 
+function generateCityList() {
+  return allCityResultsArray.map(function(city) {
+    return `
+      <article class="cityResult">
+        <p>${city}</p>
+      </article>`;
+  });
+}
 
-
+function handleGetCuisine() {
+  
+  alert(`this is working!`);
+}
 
 
 
