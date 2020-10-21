2244a87be10c2efb5731296dacac10ab16783c1f
Freddy Jimenez <freddy.junior@gmail.com>
2020-10-21 17:42:13 -0500

    added new search button functionality in restaurant list and cleaned code

--- js/script.js
+++ js/script.js
@@ -1,49 +1,93 @@
 // ***** VARIABLES AND CONSTANTS *****
-let cityResults;
-let allCityResultsArray = [];
-let CITY_INPUT;
-let cuisineResults;
-let step;
-let cuisineDropdownList;
-let CITY_ID;
-let CUISINE_ID;
-let restaurantResults;
+let step; // step in generateList()
+let cityResults; // city results
+let CITY_INPUT; // city input for API call
+let cuisineResults; // cuisine results
+let CITY_ID; // assigned city ID for API call
+let CUISINE_ID; // assigned cuisine ID for API call
+let restaurantResults; // restaurant results
 
 // ***** CACHED ELEMENT REFERENCES *****
-let $form = $('form');
-let $formInput = $('#inputBar')
-let $input = $('input[type="text"]');
-//let $formSubmit = $('input[type="submit"]');
-let $mainTitle = $('#mainTitle');
-let $mainHeader = $('.mainHeader');
-let $listEl = $('#list');
-let $cityResult;
-let $cuisineResult;
+let $form = $('form'); // input and submit button
+let $formInput = $('#inputBar') // input bar
+let $input = $('input[type="text"]'); // input text
+let $submitBtn = $('#submitBtn'); // submit button
+let $newSearch = $('#newSearch'); // new search button
+let $mainTitle = $('#mainTitle'); // main title
+let $mainHeader = $('.mainHeader'); // main header to include filter bar and title
+let $listEl = $('#list'); // everything below main header
+let $cityResult; // city result elements
+let $cuisineResult; // cuisine result elements
+
 
 // ***** EVENT LISTENERS *****
-$form.on('submit', handleGetCity);
+$form.on('submit', handleGetCity); // click submit on home page
+$newSearch.on('click', init); // click new search on restaurant list
+
 
 // ***** FUNCTIONS *****
+function init() {
+  // SETS INPUT BACK TO EMPTY
+  $input.val('');
+
+  // REPLACES MAIN TITLE TEXT BACK TO SEARCH BELOW
+  $mainTitle.text('Search below')
+
+  // PUTS MAIN HEADER BACK TO NORMAL
+  $mainHeader.css('display', '');
+  $mainHeader.css('grid-template-columns', '');
+  $mainHeader.css('width', '');
+
+  // SHOWS INPUT BAR AND SUBMIT BUTTON
+  $formInput.css('display','');
+  $submitBtn.css('display', '');
+  // TODO: WHY DO I NEED THIS HERE?
+  $submitBtn.css('margin', '0 0 0 6px');
+
+  // HIDES NEW SEARCH BUTTON
+  $newSearch.css('display', 'none');
+
+  // REFORMATS THE INPUT BAR TO ITS ORIGINAL STATE
+  $formInput.css('width','400px');
+  $formInput.css('height','50px');
+  $formInput.css('font-size','20px');
+  $formInput.css('padding','20px');
+  $formInput.css('border-radius','12px');
+
+  // REMOVES ATTRIBUTE FROM INPUT BAR SO THAT TYPEWRITER CAN FUNCTION
+  $input.attr('placeholder','');
+
+  // REMOVES ALL ELEMENTS FROM MAIN SECTION (RESTAURANT LIST)
+  $listEl.html('');
+
+  // REASSIGNS TYPEWRITER VARIABLES
+  string = '';
+  i = 0;
+
+  // RUNS TYPEWRITER
+  typeWriter();
+}
+
+
 function handleGetCity(event) {
   event.preventDefault();
   CITY_INPUT = $input.val();
 
-  // EMPTIES ARRAY AFTER EACH SUBMIT
-  allCityResultsArray = [];
-
   // ONLY RUNS CITY API IF THERE IS AN INPUT
   if (CITY_INPUT) {
     $.ajax({
+      // GET METHOD
       dataType: 'json',
+      // API URL
       url: `https://developers.zomato.com/api/v2.1/cities?q=${CITY_INPUT}`,
       headers: {
+      // API KEY
       'user-key': '8a7a179a36a4a14423468c3906678747'
       }
     })
     .then(function(response) {
     // ASSIGNS RESPONSE TO VARIABLE
     cityResults = response;
-    // RUNS DEFINE DATA FUNCTION
 
     // RENDERS CITY RESULTS
     cityResultRender();
@@ -54,16 +98,6 @@ function handleGetCity(event) {
 }
 
 
-function defineData(city) {
-  // CITY DATA
-  if(city) {
-    cityResults.location_suggestions.forEach(function(index) {
-      allCityResultsArray.push(index.name);
-    })
-    console.log('RESULTS', allCityResultsArray);
-  }
-}
-
 
 function cityResultRender() {
   // FADES OUT INPUT BAR
@@ -72,9 +106,10 @@ function cityResultRender() {
   $input.val('');
   // CHANGES TITLE TO CHOOSE YOUR CITYs
   $mainTitle.text(`Choose your city:`);
+  $mainTitle.css('text-align', 'center');
   // CHANGES STEP SO generateList() KNOWS WHAT TO DO
   step = 'city';
-  // GENERATES LIST OF CITIES FROM RESPONSE ONTO PAGE
+  // GENERATES HTML LIST OF CITIES FROM RESPONSE ONTO PAGE
   $listEl.html(generateList(step));
 
   // ***** CITY RESULT CACHED REFERENCE AND EVENT LISTENER *****
@@ -85,6 +120,7 @@ function cityResultRender() {
 }
 
 function generateList() {
+  // GENERATES HTML FOR CITY LIST
   if (step == 'city') {
     return cityResults.location_suggestions.map(function(city) {
       return`
@@ -93,18 +129,12 @@ function generateList() {
         </article>`;
       }) 
   } else if (step == 'cuisine') {
-    // NEED TO FIX THIS
-    cuisineDropdownList = cuisineResults.cuisines.map(function(cuisine) {
-      return cuisine.cuisine.cuisine_name;
-    })
-    // BRINGS BACK INPUT FOR CUISINE FILTER
-    //$form.html('<input id="inputBar" type="text" placeholder="Filter cuisine results here" onkeyup="filterFunction()">');
+    // BRINGS BACK INPUT FOR CUISINE FILTER AND ADDS FILTER ABILITY
     $form.css('display', 'flex');
     $input.attr('onkeyup', 'filterFunction()');
     $input.attr('placeholder','Filter cuisines here by name');
 
-
-    // RENDERS CUISINE LIST
+    // GENERATES HTML FOR CUISINE LIST
     return cuisineResults.cuisines.map(function(cuisine) {
       return`
         <article class="cuisineResult" data-cuisineid="${cuisine.cuisine.cuisine_id}">
@@ -112,6 +142,7 @@ function generateList() {
         </article>`;
     })
   } else if (step == 'restaurant') {
+    // GENERATES HTML FOR RESTAURANT RESULTS
     return restaurantResults.restaurants.map(function(restaurant) {
       return`
         <article class="restaurantResult" data-restaurantid="${restaurant.restaurant.id}">
@@ -130,7 +161,7 @@ function generateList() {
           <p class="secondTitle">HIGHLIGHTS</p>
           <p class="restaurantHighlights">${restaurant.restaurant.highlights.join(`, `)}</p>
           </div>
-          <div class="restaurantFooter" href="tel:8135977197">
+          <div class="restaurantFooter">
           <p class="thirdTitle" data-url="tel:${restaurant.restaurant.phone_numbers}">CALL</p>
           <p class="thirdTitle" id="right-box" data-url="${restaurant.restaurant.url}">WEBSITE</p>
           </div>
@@ -139,68 +170,51 @@ function generateList() {
   }
 }
 
-
+// FILTER FUNCTION FOR CUISINE LIST
+// THIS IS RUN ON KEYUP (AFTER EVERY KEY INPUT)
 function filterFunction() {
+  // ASSIGNS CURRENT INPUT TO A VARIABLE AND MAKES IT UPPER CASE
   let filter = $input.val().toUpperCase();
+
+  // ITEREATES THROUGH EACH CUISINE RESULT
   $cuisineResult.each(function(i) {
+    // CHECKS TO SEE IF CURRENT INPUT MATCHES ANY CUISINE RESULT NAMES
     if ($cuisineResult[i].innerText.toUpperCase().indexOf(filter) > -1)
     {
-      console.log(`${i}, this cuisine contains this value`);
+      // DISPLAYS THE CUISINE IF THERE IS A MATCH
       $(this).css('display', 'flex');
     }
     else {
+      // REMOVES THE CUISINE IF THERE IS NOT A MATCH
       $(this).css('display', 'none');
     }
-    /* if ( (this).text(indexOf(filter) > -1) {
-      $(this).css('display', 'flex');
-    } else {
-      $(this).css('display', 'none');
-    }*/
+
   })
 }
 
-/*
-
-TODO: ADD FILTER FUNTION
-ADD CUISINE BOXES
-
-function filterFunction() {
-  
-  let filter = $input.val.toUpperCase();
-  for (i = 0; i < $cuisineBoxes.length; i++) {
-    let txtValue = $cuisineBoxes[i].textContent || $cuisineBoxes[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      $cuisineBoxes[i].css('display', 'flex');
-    } else {
-      $cuisineBoxes[i].css('display', 'none');
-    }
-  }
-}
-
-
-IF selected value inner string has .indexOf('input')
-
-*/
-
-
 function handleGetCuisine(event) {
   // ASSIGN CITY SELECTED ID TO VARIABLE
-  // WHY DOES IT WORK ONLY WHEN I CONSOLE LOG THIS???????
-  console.dir(event.target);
-  console.log(event.target.dataset.cityid);
+  // console.dir(event.target); - DO NOT REMOVE
+  // console.log(event.target.dataset.cityid); - DO NOT REMOVE
   CITY_ID = event.target.dataset.cityid;
+
   // CUISINE API CALL
   if (CITY_ID) {
+    // GET METHOD
     $.ajax({
     dataType: 'json',
+    // API URL
     url: `https://developers.zomato.com/api/v2.1/cuisines?city_id=${CITY_ID}`,
     headers: {
+    // API KEY
     'user-key': '8a7a179a36a4a14423468c3906678747'
     }
     })
     .then(function(response) {
-      console.log(response);
+      // ASSIGNS RESPONSE TO VARIABLE
       cuisineResults = response;
+
+      // RENDERS CUISINE RESULTS
       renderCuisines();
     })
   } else {
@@ -214,97 +228,106 @@ function renderCuisines() {
 
   // CHANGES TITLE TO CUISINE / MAIN HEADER
   $mainTitle.text(`Choose your cuisine:`);
+  $mainTitle.css('text-align', 'left');
+
+  // SETS MAIN HEADER TO ALLOW FILTER BAR AND TITLE
   $mainHeader.css('display', 'grid');
   $mainHeader.css('grid-template-columns', '2fr 1fr');
   $mainHeader.css('width', '800px');
-  $('#submitBtn').css('display', 'none');
+
+  // HIDES SUBMIT BUTTON ON FORM
+  $submitBtn.css('display', 'none');
+
+  // MAKES INPUT BAR SMALLER TO FIT SPACE
   $formInput.css('width','300px');
   $formInput.css('height','30px');
   $formInput.css('font-size','15px');
   $formInput.css('padding','10px');
   $formInput.css('border-radius','5px');
 
+  // REFORMATS SECTION TO FIT ALL BOXES OF CUISINE
+  $listEl.css('display', 'grid');
+  $listEl.css('grid-template-columns', '1fr 1fr 1fr 1fr 1fr');
+  $listEl.css('grid-template-rows', 'auto');
+  $listEl.css('flex-wrap', 'wrap');
 
-  // UPDATES CSS TO FIT ALL BOXES OF CUISINE
-  $('#list').css('display', 'grid');
-  $('#list').css('grid-template-columns', '1fr 1fr 1fr 1fr 1fr');
-  $('#list').css('grid-template-rows', 'auto');
-  $('#list').css('flex-wrap', 'wrap');
+  // GENERATES HTML LIST OF CUISINES FROM RESPONSE ONTO PAGE
   $listEl.html(generateList(step));
 
   // ***** CITY RESULT CACHED REFERENCE AND EVENT LISTENER *****
   $cuisineResult = $('.cuisineResult');
-  // RUNS CUISINE API
+
+  // RUNS RESTAURANT API
   $cuisineResult.on('click', handleGetRestaurants);
 }
 
 
 function handleGetRestaurants(event) {
-  console.log('YOU ARE ALMOST THERE');
-  console.dir(event.target);
-  console.log(event.target.dataset.cuisineid);
+  // ASSIGN CUISINE SELECTED ID TO VARIABLE
+  //console.dir(event.target); - DO NOT REMOVE
+  //console.log(event.target.dataset.cuisineid); - DO NOT REMOVE
   CUISINE_ID = event.target.dataset.cuisineid;
 
   // FINAL RESTAURANT API CALL
   if (CUISINE_ID) {
+    // GET METHOD
     $.ajax({
     dataType: 'json',
+    // API URL
     url: `https://developers.zomato.com/api/v2.1/search?entity_id=${CITY_ID}&entity_type=city&cuisines=${CUISINE_ID}&order=asc`,
     headers: {
+    // API KEY
     'user-key': '8a7a179a36a4a14423468c3906678747'
     }
     })
     .then(function(response) {
-      console.log(response);
+      // ASSIGNS RESPONSE TO VARIABLE
       restaurantResults = response;
+
+      // RENDERS CUISINE RESULTS
       renderRestaurants();
+
+      // ***** WEBSITE/CALL CACHED REFERENCE AND EVENT LISTENER *****
       $('.thirdTitle').on('click', function(event) {
         window.open(`${event.target.dataset.url}`, '_blank');
       })
-
-      //window.open("", "MsgWindow", "width=200,height=100");
-
-
-/*    let resultCounter = response.results_found;
-      let resultStart = 20;
-      resultCounter -= 20;
-      while (resultCounter > 0) {
-        $.ajax({
-          dataType: 'json',
-          url: `https://developers.zomato.com/api/v2.1/search?entity_id=${CITY_ID}&entity_type=city&cuisines=${CUISINE_ID}&order=asc${resultStart}`,
-          headers: {
-          'user-key': '8a7a179a36a4a14423468c3906678747'
-          }
-          })
-
-      }
-
-*/
-
     })
-
 }}
 
 function renderRestaurants() {
+  // CHANGES STEP SO generateList() KNOWS WHAT TO DO
   step = 'restaurant';
+
+  // GENERATES HTML LIST OF RESTAURANTS FROM RESPONSE ONTO PAGE
   $listEl.html(generateList(step));
+
+  // REFORMATS LIST OF RESTAUNTS BACK TO FLEX COLUMN (NO GRID)
   $('#list').css('display', 'flex');
   $('#list').css('grid-template-columns', '');
   $('#list').css('grid-template-rows', '');
   $('#list').css('flex-wrap', '');
   $('#list').css('flex-direction', 'column');
-  $form.css('display', 'none');
+
+  // REMOVES INPUT BAR / SUBMIT BUTTON FROM SCREEN
+  $formInput.css('display', 'none');
+  $submitBtn.css('display', 'none');
+
+  // ADDS NEW SEARCH BUTTON AND ALIGNS RIGHT
+  $newSearch.css('display', '');
+  $form.css('justify-content', 'flex-end');
 
   // CACHED RESTAURANT ELEMENTS
   $restaurants = $('article');
-  $restaurantHeader = $('.restaurantHeader');
-  $restaurantMain = $('.restaurantMain');
-  $restaurantFooter = $('.restaurantFooter');
   $restaurantName = $('.restaurantName');
   $restaurantLoc = $('.restaurantLoc');
   $restaurantRating = $('.restaurantRating');
   $restaurantHighlights = $('.restaurantHighlights');
- 
+
+  // EACH RESTAURANT RESULT HAS A (HEADER/MAIN/FOOTER CACHED)
+  $restaurantHeader = $('.restaurantHeader');
+  $restaurantMain = $('.restaurantMain');
+  $restaurantFooter = $('.restaurantFooter');
+
   // STYLING RESTAURANT LIST
   $restaurants.css('font-size', '10px');
   $restaurants.css('color', 'black');
@@ -315,10 +338,8 @@ function renderRestaurants() {
   $restaurants.css('grid-template-columns', '1fr');
   $restaurants.css('grid-template-rows', '1fr 1fr 50px');
   $mainTitle.text(`Results (${restaurantResults.results_shown})`);
-  //$mainTitle.css('width', '600px');
   $mainHeader.css('width', '600px');
 }
-
 
 
 // ***** YEAR ON COPYRIGHT *****
@@ -351,29 +372,3 @@ function typeWriter() {
       setTimeout(typeWriter, speed);
   }
 }
-
-
-// ******** PROXY SERVER TESTING ********
-
-$.ajax('https://proxy-fork.herokuapp.com/city-api')
-.then(function(data) {
-  console.log(data);
-}, function(error) {
-  console.log(error);
-})
-
-/*
-
-$.ajax({
-  dataType: 'json',
-  url: `https://developers.zomato.com/api/v2.1/search?entity_id=292&entity_type=city&cuisines=1&establishment_type=18&sort=cost&start=20&count=20`,
-  headers: {
-  'user-key': '8a7a179a36a4a14423468c3906678747'
-  }
-  })
-  .then(function(response) {
-    console.log(response);
-  });
-
-
-  */
